bdccaf1f1ee8ac089cf95b288b04e01b52e47f5f
Steven Hernandez Rios <estevenhr@hotmail.com>
2017-10-07 11:25:45 -0500

    Inicio parte grafica

---
+++ src/RegistroUsuario.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import javax.swing.*;
+public class RegistroUsuario extends JPanel {
+
+		public RegistroUsuario() {
+			
+		}
+}
---
+++ src/VentanaPrincipal.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class VentanaPrincipal extends JFrame{
+	 
+	public VentanaPrincipal() {
+		
+	}
+}
